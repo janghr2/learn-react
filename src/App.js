223505b7c4800ef1c6b128cfa787ec7e0c689829
janghr2 <wkdgpfls09932@gmail.com>
2026-09-16 13:20:45 +0900

    greeting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import Greeting from "./Greeting"
 
 function App() {
+
+// logic
+// 구조분해할당 - object destructure
+
+const animals = ["dog", "cat", "pig"]
+// const second = animals[1]
+const [first, second, third, forth] = animals
+
+console.log("🚀 ~ App ~ first:", first)
+
+
+console.log("🚀 ~ App ~ second:", second)
+
+
+// const todoitem = {
+//   todo: "react 학습하기"
+//   iscomplete: false
+// }
+
+// const { todo: iscomplete, "react 학습하기"} = todoitem
+
+
+
+
+//view
   return (
     <div className="App">
       Hello world
-    </div>
-  );
+      <Greeting />
+      
+    </div>    
+  )
 }
+
+
+  
+
+
+
+
 
 export default App;
---
+++ src/Greeting.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Greeting = () => {
+  return ( 
+  <div style= {{"backgroundColor" : "blue", "padding" :"10px" }}> Greeting 
+  </div>
+  )
+}
+
+export default Greeting
